1e8f912c7557b8eac00ddacd9491c02b272febd1
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-11-16 19:09:11 +0500

    Tasks solved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public static void main(String[] args) {
     try {
-        UserValidation.validateUser("user_name", "Password123", "Password123");
+        UserValidation.validateUser("user_name_Roman", "Password132", "Password321");
         System.out.println("Пользователь успешно проверен.");
     } catch (WrongLoginException | WrongPasswordException e) {
         System.err.println("Ошибка: " + e.getMessage());
